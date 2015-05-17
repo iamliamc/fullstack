1502b095f5007640d2f9e28cdc3fe5194c93fa4c
Liam <ljc3179@gmail.com>
2015-05-16 20:29:32 -0400

    Added query and display for restaurant url

--- menu/webserver2.py
+++ menu/webserver2.py
@@ -1,11 +1,34 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
-
+## import CRUD operations
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+## create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 class webServerHandler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         try:
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                
+                output = ""
+                output += "<html><body>"
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+                output += "</html></body>"
+                self.wfile.write(output)
+                return
+                
+                
             if self.path.endswith("/hello"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -14,6 +37,9 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<html><body>"
                 output += "<h1>Hello!</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                while n <= len(allRestaurants):
+                    output += "<h2> %s </h2>" % allRestaurants[n].name
+                    n += 1
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
@@ -40,11 +66,11 @@ class webServerHandler(BaseHTTPRequestHandler):
             self.send_response(301)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
                 messagecontent = fields.get('message')
+                
             output = ""
             output += "<html><body>"
             output += " <h2> Okay, how about this: </h2>"
